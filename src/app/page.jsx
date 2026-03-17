b5ece35636add8b5361bc78af83b0f6083c39e5d
ayushkumar21161 <ayush@homeasy.io>
2026-03-17 18:17:03 +0530

    Updated project

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -411,7 +411,7 @@ export default function Home() {
               variants={imageReveal}
               className="lg:col-span-5 relative"
             >
-              <div className="absolute -bottom-6 -left-6 w-32 h-32 bg-blue-100 rounded-full blur-2xl"></div>
+              <div className="absolute -bottom-6 -left-6 w-32 h-32 bg-green-100 rounded-full blur-2xl"></div>
 
               <div className="relative rounded-2xl overflow-hidden border-4 border-white shadow-2xl max-w-[420px] w-full mx-auto">
 
@@ -422,7 +422,7 @@ export default function Home() {
                 src="https://aixpertslabs.com/assets/pradeep_kumar-69a6af88.jpeg"
               />
 
-              <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-blue-700/90 to-transparent p-6 text-white">
+              <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t to-transparent p-6 text-white">
                 <h3 className="text-2xl font-bold">Pradeep Kumar</h3>
                 <p className="text-green-300 text-sm font-bold uppercase tracking-widest">
                   Founder & CEO
